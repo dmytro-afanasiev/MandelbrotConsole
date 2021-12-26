d0f8ee2c2699b71e9563a646667e56a1f3982c84
Dmytro Afanasiev <duma12let@gmail.com>
2021-12-26 19:32:49 +0200

    Add an image and build command from code blocks

---
+++ build_and_execute.sh
@@ -0,0 +1,3 @@
+g++ -Wall -fexceptions -g -Iinclude -c /home/dimon/MandelbrotConsole/src/Fractal.cpp -o obj/Debug/src/Fractal.o
+g++  -o bin/Debug/MandelbrotConsole obj/Debug/main.o obj/Debug/src/App.o obj/Debug/src/Fractal.o
+./bin/Debug/MandelbrotConsole 
